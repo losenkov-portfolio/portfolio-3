21941e14ac955a69d5a3cfe306a885abb89ccc08
Vadim Losenkov <vprog0729@gmail.com>
2021-03-19 21:51:39 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(function() {
+  $('.reviews__wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    dots: true,
+    prevArrow: '<div class="arrow arrow-prev"></div>',
+    nextArrow: '<div class="arrow"></div>',
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+    ]
+  })
+  $('.header__btn').on('click', () => {
+    $('.header__btn').toggleClass('open')
+    $('.over').toggleClass('open')
+    $('.header__mobile').toggleClass('open')
+  })
+  $('.header__list-link').on('click', () => {
+    $('.header__btn').removeClass('open')
+    $('.over').removeClass('open')
+    $('.header__mobile').removeClass('open')
+  })
+  $('.over').on('click', () => {
+    $('.header__btn').removeClass('open')
+    $('.over').removeClass('open')
+    $('.header__mobile').removeClass('open')
+  })
+})
